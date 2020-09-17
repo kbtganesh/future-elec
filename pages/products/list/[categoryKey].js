607593c62720668d643bf3d7d43f806c719d1aaa
Murugan <Ganeshbabu.Murugan@agilysys.com>
2020-09-17 08:49:45 +0530

    added loadMore products

--- pages/products/list/[categoryKey].js
+++ pages/products/list/[categoryKey].js
@@ -4,19 +4,27 @@ import Router, { withRouter } from 'next/router'
 import { ProductContext } from "../../../product-context";
 import "./list.scss";
 import Snackbar from '@material-ui/core/Snackbar';
-
+import Button from "components/CustomButtons/Button.js";
+const base = {
+    local: 'http://localhost:5001/eeradi/us-central1',
+    firebase: 'https://us-central1-eeradi.cloudfunctions.net',
+}
+const DOMAIN = base.firebase;
 function List({ response, params, query }) {
     console.log("kbt: List -> query", query);
     console.log("kbt: List -> params", params);
-    let { success, data, errorMessage } = response || {};
+    let { success, data: responseData, errorMessage } = response || {};
     let { categoryKey } = params;
     console.log("kbt: List -> categoryKey", categoryKey);
     const { product, setProduct, category, setTitle, childCategories } = useContext(ProductContext);
     console.log("kbt: List -> childCategories", childCategories);
-    const [count, setCount] = useState(0);
-    const [parent, setParent] = useState();
+    // actual data to be rendered
+    const [data, setData] = useState(responseData);
+    const [message, setMessage] = useState(0);
+    const [loadMoreBtn, setLoadMoreBtn] = useState(true);
     console.log("kbt: List -> childCategories[categoryKey]", childCategories[categoryKey]);
     const [child, setChild] = useState(query && query.child || '');
+    const [scroll, setScroll] = useState({x: 0, y: 0});
     console.log("kbt: List -> child", child);
 
     useEffect(() => {
@@ -26,7 +34,14 @@ function List({ response, params, query }) {
     useEffect(() => {
         hideLoader();
         setChild(query && query.child || '');
+        setLoadMoreBtn(true);
+        setData(responseData);
     }, [query]);
+
+    useEffect(() => {
+        scrollTo(scroll.x, scroll.y);
+        setLoadMoreBtn(true);
+    }, [data])
     // async function fetchData() {
     //     const res = await fetch('https://us-central1-eeradi.cloudfunctions.net/api/products')
     //     setData(await res.json());
@@ -62,15 +77,39 @@ function List({ response, params, query }) {
     }
 
     function onClickChildCategory(child) {
+        setScroll({x: 0, y: 0});
         setChild(child);
         showLoader();
         Router.push(`/products/list/[categoryKey]?child=${child}`, `/products/list/${categoryKey}?child=${child}`)
             .then(_ => window.scrollTo(0, 0));
     }
+
+    async function loadMoreProducts() {
+        let lastitem = data.length ? data[data.length - 1] : '';
+        let child = query.child && query.child !== 'all' ? query.child : '';
+        let trackId = lastitem && lastitem.id || '';
+        let url = `${DOMAIN}/api/products/category/${params.categoryKey}` +
+            `?child=${child}&trackId=${trackId}`;
+        let rawres = await fetch(url);
+        let res = await rawres.json();
+        if(res.success && res.data && res.data.length > 0) {
+            setScroll({x: 0, y: scrollY});
+            setData(pd => ([...pd, ...res.data]));
+        }else {
+            if(res.errorCode == 1001)
+            setLoadMoreBtn(false);
+            setMessage('No more products available');
+        }
+    }
+
+    function onCloseMessage() {
+        setMessage('');
+    }
+    
     const childCategoryList = childCategories && childCategories[categoryKey] || [];
     return (
         <div className="page-container">
-            <Snackbar open={!!errorMessage} autoHideDuration={4000} message={errorMessage}>
+            <Snackbar key={message} open={!!message} autoHideDuration={4000} message={message} onClose={onCloseMessage}>
             </Snackbar>
             {childCategoryList && childCategoryList.length > 0 && <div className="filterByChildCategory">
                 <div className={`childCategory ${child == 'all' ? 'selected' : ''}`} onClick={() => onClickChildCategory('all')}>All</div>
@@ -108,6 +147,9 @@ function List({ response, params, query }) {
                     )
                 })}
             </div>}
+            {data?.length > 0 && <div className="loadMore">
+                {loadMoreBtn && <Button className="loadMoreBtn" onClick={loadMoreProducts}>Load More</Button>}
+            </div>}
             {/* <div className="child-category">
                 {parent && childCategories[parent].map((c, i) => (
                     <Grow in={true} timeout={(i+1)*250}>
@@ -129,8 +171,8 @@ export async function getServerSideProps({ params, query }) {
     let response = {};
     try {
         console.log("***********")
-        console.log("URL", 'https://us-central1-eeradi.cloudfunctions.net/api/products/category/' + categoryKey + queryParam);
-        const res = await fetch('https://us-central1-eeradi.cloudfunctions.net/api/products/category/' + categoryKey + queryParam)
+        console.log("URL", `${DOMAIN}/api/products/category/` + categoryKey + queryParam);
+        const res = await fetch(`${DOMAIN}/api/products/category/` + categoryKey + queryParam)
         response = await res.json();
     } catch (e) {
         response.errorMessage = e.errorMessage || 'Unknown Error Occured';
